c2b43dc0296a9dd16c793753c9fa6aaa49fddf0f
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-10-18 09:50:14 +0300

    add

--- n.js
+++
@@ -1,7 +0,0 @@
-
-    var fileInput = document.getElementById('file').value;
-  
-    var in1 = document.getElementById('inner');
-
-    fileInput.onchange = () => in1.innerHTML = `<video controls width="400"><source src="${URL.createObjectURL(fileInput.files[0])}" type="${fileInput.files[0].type}">Your browser does not support the video tag.</video>`;
-  
